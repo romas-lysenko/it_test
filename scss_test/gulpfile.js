8733eda433aa0e1de4e3e075cca50d277bc56721
Roman <lysenko_r_5@mail.ru>
2019-04-04 15:24:32 +0300

    dz optimization dinosaur

--- scss_test/gulpfile.js
+++ scss_test/gulpfile.js
@@ -2,13 +2,23 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var imagemin = require ('gulp-imagemin');
 
 sass.compiler = require('node-sass');
+
+
+
 
 gulp.task('sass', function () {
     return gulp.src('./scss/main.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./css'));
 });
+
+gulp.task('default', () =>
+    gulp.src('img/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/images'))
+);
 
 
